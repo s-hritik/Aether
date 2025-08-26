884a7ede502c670522ceb71ae76d87c21dfce279
s-hritik <hritiksingh158@gmail.com>
2025-08-27 03:24:35 +0530

    Remove all incorrect  files

--- backend/src/routes/payment.route.js
+++
@@ -1,9 +0,0 @@
-// import {Router} from 'express';
-// import {createPaymentIntent} from '../controllers/payment.controller.js';
-// import {verifyJWT} from '../middlewares/auth.middleware.js';
-
-// const payment = Router();
-
-// payment.route('/create-intent').post(verifyJWT, createPaymentIntent);
-
-// export default payment;
