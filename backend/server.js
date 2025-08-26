1b816a2be7c9b6bc1be2868a6ffe50ebc8afb259
s-hritik <hritiksingh158@gmail.com>
2025-08-27 04:33:43 +0530

    fix: Update server config and gitignore files

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,31 @@
 import dotenv from 'dotenv';
-dotenv.config(
-     { path: './.env'}
-);
+dotenv.config({ path: './.env' });
 
+import http from 'http'; 
 import https from 'https';
 import fs from 'fs';
-import connectDB from './src/config/database.js';
-import {app} from './src/app.js';
-
-// Read the certificate files 
-const httpsOptions = {
-  key: fs.readFileSync('./localhost-key.pem'),
-  cert: fs.readFileSync('./localhost.pem')
-};
+import connectDB from './config/database.js';
+import { app } from './src/app.js';
 
 connectDB()
-.then(() => {
-
+  .then(() => {
     const port = process.env.PORT || 8000;
-    https.createServer(httpsOptions , app).listen(port , () => {
-        console.log(`Server is running on port : ${port}`);
-    })
-})
-.catch ((error) => {
+
+    if (process.env.NODE_ENV === 'production') {
+      http.createServer(app).listen(port, () => {
+        console.log(`✅ Server is running on port: ${port}`);
+      });
+    } else {
+      const httpsOptions = {
+        key: fs.readFileSync('./localhost-key.pem'),
+        cert: fs.readFileSync('./localhost.pem')
+      };
+      https.createServer(httpsOptions, app).listen(port, () => {
+        console.log(`✅ Server is running securely on: https://localhost:${port}`);
+      });
+    }
+  })
+  .catch((error) => {
     console.log('MongoDB connection failed:', error.message);
     process.exit(1);
-})
+  });
